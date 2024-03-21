5f99c05bafa81d9022dd0337db77da69409d7dad
Евгений Н <strtoid.zak2018@yandex.ru>
2024-03-21 11:25:33 +0300

    Закончил отображение информации клиента

--- Northwind.Web/Pages/CustomerInfo.cshtml.cs
+++ Northwind.Web/Pages/CustomerInfo.cshtml.cs
@@ -6,11 +6,35 @@ namespace Northwind.Web.Pages
 {
     public class CustomerInfoModel : PageModel
     {
+        #region Fields
         /// <summary>
         /// Клиент
         /// </summary>
         [BindProperty]
         public Customer? customer { get; set; }
+
+        /// <summary>
+        /// Заказ клиента
+        /// </summary>
+        [BindProperty]
+        public Order? order { get; set; }
+
+        /// <summary>
+        /// Компания перевозки 
+        /// </summary>
+        [BindProperty]
+        public Shipper shipper { get; set; }
+
+        /// <summary>
+        /// Список заказов
+        /// </summary>
+        public IEnumerable<Order> _orders;
+
+        /// <summary>
+        /// Список компаний доставки
+        /// </summary>
+        public IEnumerable<Shipper> _shippers;
+
         /// <summary>
         /// Id клиента
         /// </summary>
@@ -19,14 +43,39 @@ namespace Northwind.Web.Pages
         /// Контекст БД
         /// </summary>
         private NorthwindContext _db;
+
+        private Shipper undefinedShipper = new()
+        {
+            CompanyName = "NONE"
+        };
+        #endregion
+        #region Ctor
         public CustomerInfoModel(NorthwindContext db)
         {
             _db = db;
         }
+        #endregion
         public void OnGet(string custId)
         {
+            ViewData["Title"] = "Northwind B2B";
             customerId = custId;
             customer = _db.Customers.Where(c => c.CustomerId == customerId).FirstOrDefault();
+            _orders = _db.Orders.Where(o => o.CustomerId == customerId).OrderBy(o => o.OrderId).ToList();
+        }
+        /// <summary>
+        /// Предоставит курьра
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <returns></returns>
+        public Shipper GetShipperById(int? Id)
+        {
+            if (Id is null)
+                return undefinedShipper;
+            Shipper shipper = _db.Shippers.Where(s => s.ShipperId == Id).FirstOrDefault();
+            if (shipper is not null)
+                return shipper;
+            else
+                return undefinedShipper;
         }
     }
 }
