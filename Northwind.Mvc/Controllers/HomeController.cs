0d5de96731fd70ac9ce521512a604c595a6a4104
Евгений Н <strtoid.zak2018@yandex.ru>
2024-03-28 17:53:48 +0300

    Тест методов разметки вспомогательного класса представления

--- Northwind.Mvc/Controllers/HomeController.cs
+++ Northwind.Mvc/Controllers/HomeController.cs
@@ -43,5 +43,39 @@ namespace Northwind.Mvc.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        /// <summary>
+        /// Отображение информации о продукте
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public IActionResult ProductDetail(int? id)
+        {
+            if (!id.HasValue)
+            {
+                return BadRequest("Вы должны указать id продукта при обращении к странице");
+            }
+            Product? model = _db.Products.SingleOrDefault(p => p.ProductId == id);
+            if (model == null)
+            {
+                return NotFound($"ProductId {id} не найден");
+            }
+            return View(model); // передаём модель для просмотра и возвращаем результат
+        }
+
+        public IActionResult ModelBinding()
+        {
+            return View(); // страница с формой
+        }
+        [HttpPost]
+        public IActionResult ModelBinding(Thing thing)
+        {
+            //return View(thing); // привязанный к моделти объект
+            HomeModelBindingViewModel model = new(
+                thing,
+                !ModelState.IsValid,
+                ModelState.Values.SelectMany(state => state.Errors).Select(error => error.ErrorMessage));
+            return View(model);
+        }
     }
 }
